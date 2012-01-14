b6e8dfbd2a079107d5b4f76003e5d59b589bdad8
Zohar Babin <zohar.babin@kaltura.com>
2012-01-14 11:44:58 -0500

    .

--- Vidyo2KalturaConfig.php
+++ Vidyo2KalturaConfig.php
@@ -5,7 +5,7 @@ class Vidyo2KalturaConfig
 	const SECRET = 'cb867184ae0788bbe149f800162d0c0c';
 	const ADMIN_SECRET = 'ed606aad2c9a9dec6a997682a6d40700';
 	const SERVICE_URL = 'http://www.kaltura.com';
-	const KALTURA_VIDYO_RECORDINGS_CATEGORY = 'MediaSpace>VidyoRecordings, ';
+	const KALTURA_VIDYO_RECORDINGS_CATEGORY = 'MediaSpace>VidyoRecordings>Uncategorized';
 	const VIDYO_KALTURA_TAGS = 'vidyorecording';
 	const VIDYO_USER = 'zohar';
 	const VIDYO_PASSWORD = 'zohar';
