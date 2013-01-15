74b15f314ddd4be66303f0d8401ad9bd4ac5901b
Zohar Babin <zohar.babin@kaltura.com>
2013-01-15 00:17:26 +0000

    added a status function to the run daemon

--- rund.sh
+++ rund.sh
@@ -3,6 +3,17 @@
 #place in /etc/rc.local the following line to make syncer run at startup:
 #bash pathto/VidyoReplayToKaltura/rund.sh &
 
+#provide a status function:
+if [ "$1"=="status" ]; then
+        PID=`ps -eaf | grep syncv2k.php | grep -v grep | awk '{ print $2 }'`
+        if [[ $PID ]]; then
+                echo 'Syncer daemon is running: '$PID
+        else
+                echo 'daemon is not running'
+        fi
+	exit 0
+fi
+
 #first, kill any instance previously running:
 bash killd.sh reset
 #run the sync script in an infinite loop to keep it running forever
